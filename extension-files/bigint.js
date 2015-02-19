aae5dddb3dbac766a783c4485a150be16ad74256
Andrew Sun <as-com@users.noreply.github.com>
2015-02-19 17:54:38 -0500

    Update bigint.js

--- extension-files/bigint.js
+++ extension-files/bigint.js
@@ -23,11 +23,8 @@
                 return JSON.stringify(mult(one, two));
             case 'mod':
                 return JSON.stringify(mod(one, two));
-            case '^':
-                for (var i = 0; i < parseInt(bigInt2str(two)) - 1; i++) {
-                    one = mult(one, one);
-                }
-                return JSON.stringify(one);
+            case 'gcd':
+                return JSON.stringify(GCD(one, two));
         }
     }
     ext.bigIntToText = function(input, base) {
@@ -45,6 +42,15 @@
                 return greater(two, one);
         }
     }
+    ext.oneBoolOp = function(input, op) {
+        var a = JSON.parse(input);
+        switch (op) {
+            case "zero":
+                return isZero(a);
+            case "negative":
+                return negative(a);
+        }
+    }
     
     // Block and block menu descriptions
     var descriptor = {
@@ -52,11 +58,13 @@
             ['r', '฿ from text %s base %n', 'createBigInt', "9001", "10"],
             ['r', '฿ %s %m.operations %s', 'operation', "", "+", ""],
             ['b', '฿ %s %m.boolOps %s', 'boolOp', '', '=', ''],
+            ['b', '฿ is %s %m.oneBoolOps', 'oneBoolOp', '', 'zero'],
             ['r', '฿ to text %s base %n', 'bigIntToText', "", "10"]
         ],
         menus: {
             operations: ['+', '-', '*', '^', 'mod'],
-            boolOps: ['=', '>']
+            boolOps: ['=', '>'],
+            oneBoolOps: ['zero', 'negative']
         },
         url: 'https://as-com.github.io/scratch-comp09-toolbelt/'
     };
