1f292a6821cc8844d018cada5b94b03ecc8bbc8a
Andrew Sun <as-com@users.noreply.github.com>
2015-02-19 08:10:22 -0500

    Update browser.js

--- extension-files/browser.js
+++ extension-files/browser.js
@@ -20,28 +20,24 @@
         return toolbeltReady;
     };
     
-    ext.set_localstorage = function (data) {
-        localStorage.setItem(app.projectModel.id.toString(), data);
-    };
-    ext.change_localstorage = function(change) {
-        var data = localStorage.getItem(app.projectModel.id.toString());
-        if (!isNaN(parseFloat(data))) {
-             localStorage.setItem(app.projectModel.id.toString(), parseFloat(data) + change);
-        }
-    };
-    ext.get_localstorage = function () {
-        return localStorage.getItem(app.projectModel.id.toString());
-    };
-
+    ext.setCookie = function(name, data) {
+        localStorage.setItem(app.projectModel.id.toString() + "_" + name, data);
+    }
+    ext.getCookie = function(name) {
+        return localStorage.getItem(app.projectModel.id.toString() + "_" + name);
+    }
+    ext.deleteCookie = function(name) {
+        localStorage.removeItem(app.projectModel.id.toString() + "_" + name);
+    }
 
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
             // Block type, block name, function name, param1 default value, param2 default value
             ['b', 'toolbelt loaded?', 'isReady'],
-            ['', 'set local data to %s', 'set_localstorage', '0'],
-            ['', 'change local data by %d', 'change_localstorage', 1],
-            ['r', 'local data', 'get_localstorage'],
+            ['', 'set cookie %s to %s', 'setCookie', 'name', 'data'],
+            ['', 'delete cookie %s', 'deleteCookie', 'name'],
+            ['r', 'cookie %s', 'getCookie', 'name']
         ],
         url: 'https://as-com.github.io/scratch-comp09-toolbelt/'
     };
