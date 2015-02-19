e9c5ff04691dac62d8e57d5a23a1c94f22df068d
Andrew Sun <as-com@users.noreply.github.com>
2015-02-19 00:17:51 -0500

    Update crypto.js

--- extension-files/crypto.js
+++ extension-files/crypto.js
@@ -11,11 +11,63 @@
     ext.hash = function(string, algo) {
         return CryptoJS[algo](string).toString(CryptoJS.enc.Hex);
     }
+    
+    var JsonFormatter = {
+        stringify: function (cipherParams) {
+            // create json object with ciphertext
+            var jsonObj = {
+                ct: cipherParams.ciphertext.toString(CryptoJS.enc.Base64)
+            };
+
+            // optionally add iv and salt
+            if (cipherParams.iv) {
+                jsonObj.iv = cipherParams.iv.toString();
+            }
+            if (cipherParams.salt) {
+                jsonObj.s = cipherParams.salt.toString();
+            }
+
+            // stringify json object
+            return JSON.stringify(jsonObj);
+        },
+
+        parse: function (jsonStr) {
+            // parse json string
+            var jsonObj = JSON.parse(jsonStr);
+
+            // extract ciphertext from json object, and create cipher params object
+            var cipherParams = CryptoJS.lib.CipherParams.create({
+                ciphertext: CryptoJS.enc.Base64.parse(jsonObj.ct)
+            });
+
+            // optionally extract iv and salt
+            if (jsonObj.iv) {
+                cipherParams.iv = CryptoJS.enc.Hex.parse(jsonObj.iv)
+            }
+            if (jsonObj.s) {
+                cipherParams.salt = CryptoJS.enc.Hex.parse(jsonObj.s)
+            }
+
+            return cipherParams;
+        }
+    };
+    
+    ext.aesEncrypt = function(plaintext, passphrase) {
+        return CryptoJS.AES.encrypt(plaintext, passphrase, { format: JsonFormatter });
+    }
+    ext.aesDecrypt = function(ciphertext, passphrase) {
+        decrypted = CryptoJS.AES.decrypt(ciphertext, passphrase, { format: JsonFormatter });
+        if (decrypted.sigBytes < 0) {
+            return "fail";
+        }
+        return decrypted.toString(CryptoJS.enc.Utf8);
+    }
 
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
-            ['r', 'hash of %s with %m.hashes', 'hash', "hello world"],
+            ['r', 'hash %s with %m.hashes', 'hash', "hello world"],
+            ['r', 'aes %s with passphrase %s', "aesEncrypt", "message", "secret"]
         ],
         menus: {
             hashes: ["MD5", "SHA1", "SHA256", "SHA512", "SHA3", "RIPEMD160"]
