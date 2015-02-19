e256d288791f255593aef7f0cb19ec4279661222
Andrew Sun <as-com@users.noreply.github.com>
2015-02-19 17:46:03 -0500

    Update bigint.js

--- extension-files/bigint.js
+++ extension-files/bigint.js
@@ -11,7 +11,7 @@
     ext.createBigInt = function(number, base) {
         return JSON.stringify(str2bigInt(number, base));
     }
-    ext.bigIntOperation = function(a, op, b) {
+    ext.operation = function(a, op, b) {
         var one = JSON.parse(a);
         var two = JSON.parse(b);
         switch (op) {
@@ -21,21 +21,42 @@
                 return JSON.stringify(sub(one, two));
             case "*":
                 return JSON.stringify(mult(one, two));
+            case 'mod':
+                return JSON.stringify(mod(one, two));
+            case '^':
+                for (var i = 0; i < parseInt(bigInt2str(two)) - 1; i++) {
+                    one = mult(one, one);
+                }
+                return JSON.stringify(one);
         }
     }
     ext.bigIntToText = function(input, base) {
         return bigInt2str(JSON.parse(input), base);
+    }
+    ext.boolOp = function(a, op, b) {
+        var one = JSON.parse(a);
+        var two = JSON.parse(b);
+        switch (op) {
+            case "=":
+                return equals(one, two);
+            case '>':
+                return greater(one, two);
+            case '<':
+                return greater(two, one);
+        }
     }
     
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
             ['r', '฿ from text %s base %n', 'createBigInt', "9001", "10"],
-            ['r', '฿%s %m.operations %s', 'bigIntOperation', "", "+", ""],
+            ['r', '฿ %s %m.operations %s', 'operation', "", "+", ""],
+            ['b', '฿ %s %m.boolOps %s', 'boolOp', '', '=', ''],
             ['r', '฿ to text %s base %n', 'bigIntToText', "", "10"]
         ],
         menus: {
-            operations: ['+', '-', '*']
+            operations: ['+', '-', '*'],
+            boolOps: ['=', '>']
         },
         url: 'https://as-com.github.io/scratch-comp09-toolbelt/'
     };
