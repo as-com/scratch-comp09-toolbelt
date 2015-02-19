ac2b44b7f4f6f5e1406b7fc98dd4045945e5b881
Andrew Sun <as-com@users.noreply.github.com>
2015-02-19 09:06:31 -0500

    Update browser.js

--- extension-files/browser.js
+++ extension-files/browser.js
@@ -29,12 +29,41 @@
     ext.deleteCookie = function(name) {
         localStorage.removeItem("comp09toolbelt_cookie_" + app.projectModel.id.toString() + "_" + name);
     }
+    
+    ext.vexAlert = function(text, ok, callback) {
+        vex.dialog.buttons.YES.text = ok;
+        vex.dialog.alert({
+            message: text,
+            callback: callback
+        });
+    }
+    ext.vexConfirm = function(text, ok, cancel, callback) {
+        vex.dialog.buttons.YES.text = ok;
+        vex.dialog.buttons.NO.text = cancel;
+        vex.dialog.confirm({
+            message: text,
+            callback: callback
+        });
+    }
+    ext.vexPrompt = function(text, placeholder, ok, cancel, callback) {
+        vex.dialog.buttons.YES.text = ok;
+        vex.dialog.buttons.NO.text = cancel;
+        vex.dialog.prompt({
+            message: text,
+            placeholder: placeholder,
+            callback: callback
+        });
+
+    }
 
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
             // Block type, block name, function name, param1 default value, param2 default value
             ['b', 'toolbelt loaded?', 'isReady'],
+            ['w', 'alert %s', 'vexAlert', 'text'],
+            ['B', 'confirm %s true: %s false: %s', 'vexConfirm', 'text', 'Ok', 'Cancel'],
+            ['B', 'prompt %s placeholder: %s ok: %s cancel %s', 'vexPrompt', 'text', 'answer', 'Ok', 'Cancel'],
             ['', 'set cookie %s to %s', 'setCookie', 'name', 'data'],
             ['', 'delete cookie %s', 'deleteCookie', 'name'],
             ['r', 'cookie %s', 'getCookie', 'name']
