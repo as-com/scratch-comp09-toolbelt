edd4c49cc2da67aca2e646d913633d464f6ec9d0
Andrew Sun <as-com@users.noreply.github.com>
2015-02-28 21:55:21 -0500

    Update image.js

--- extension-files/image.js
+++ extension-files/image.js
@@ -39,7 +39,7 @@
         green = imageData.data[((y*(imageData.width*4)) + (x*4)) + 1];
         blue = imageData.data[((y*(imageData.width*4)) + (x*4)) + 2];
         
-        return red * 65535 + green * 256 + blue;
+        return red * 65535 + blue * 256 + green;
     }
     ext.imageWidth = function() {
         return imageData.width;
