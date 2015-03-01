5c20e558f2a8b8c02ae4f5b0dd5e7166a22785d6
Andrew Sun <as-com@users.noreply.github.com>
2015-02-28 21:24:14 -0500

    Update image.js

--- extension-files/image.js
+++ extension-files/image.js
@@ -8,13 +8,38 @@
         return {status: 2, msg: 'Ready'};
     };
     
-    var img = new Image();
+    var crossOrigin = "Anonymous";
+    //var pix = null;
+    
+    ext.setCrossDomain = function(input) {
+        crossOrigin = input;
+    }
+    ext.loadImage = function(url, callback) {
+        var canvas = document.createElement("canvas");
+        var ctx = canvas.getContext("2d");
+        var src = url;
+        var img = new Image;
+        img.crossOrigin = crossOrigin;
+        
+        img.onload = function() {
+            canvas.width = img.width;
+            canvas.height = img.height;
+            ctx.drawImage(img, 0, 0);
+            pix = ctx.getImageData(0, 0, img.width, img.height).data;
+        }
+        img.src = src;
+        if ( img.complete || img.complete === undefined ) {
+            img.src = "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==";
+            img.src = src;
+        }
+    }
     
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
             // Block type, block name, function name, param1 default value, param2 default value
-            ['', 'load crossdomain %s', 'loadcrossdomain', "anonymous"],
+            ['', 'set crossdomain to %s', 'setCrossDomain', "Anonymous"],
+            ['w', 'load image %s', 'loadImage', "http://i.imgur.com/wIOiMi4.jpg"]
         ],
         url: 'https://as-com.github.io/scratch-comp09-toolbelt/'
     };
@@ -23,4 +48,4 @@
     ScratchExtensions.register('comp09 - image', descriptor, ext);
     
 })({});
-vex.defaultOptions.className = 'vex-theme-os';
+var pix;
